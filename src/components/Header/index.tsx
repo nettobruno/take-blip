2340ef06ee801b44183c91453678a424d3d1b964
Bruno Netto <brunonettomac@outlook.com>
2022-09-02 20:49:09 -0300

    fix(header): fix eslint warning

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import logoTakeBlip from '../../assets/logo.png'
 export default function Header() {
   return (
     <Container>
-      <a href="https://www.take.net/" target='_blank'>
+      <a href="https://www.take.net/" target='_blank' rel="noopener noreferrer">
         <img src={logoTakeBlip} alt="Logo Take Blip" />
       </a>
     </Container>
