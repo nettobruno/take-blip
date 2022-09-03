95cab6ffd2ab9ff6ba2844aa6315c43ebd6ccffe
Bruno Netto <brunonettomac@outlook.com>
2022-09-03 16:49:54 -0300

    feat(profilepage): add profile page styling

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,11 +1,117 @@
-import { Container } from './style';
+import Header from '../../components/Header';
+import { ChatCircle } from 'phosphor-react';
+import { Container, Grid, Circle, Footer } from './style';
+import illustration from '../../assets/illustration.png'
+import iconMessageReceived from '../../assets/message-received.png'
+import iconMessageSend from '../../assets/message-send.png'
+import iconUserActive from '../../assets/user-active.png'
 
-function Profile() {
- return (
-  <Container>
-    <h1>ProfilePage</h1>
-  </Container>
- );
+// interface ItemsData {
+//   created: string;
+//   name: string;
+//   type: string;
+// }
+
+// interface LocationState {
+//   name: {
+//     name: string;
+//   };
+// }
+
+export default function Profile() {
+  // const location = useLocation();
+  // const [profiles, setProfiles] = useState<ItemsData[]>([]);  
+
+  // useEffect(() => {
+  //   const { name }  = location.state as LocationState;
+
+  //   api.get(`/${name}/details`).then((response) => {
+  //     console.log(response.data);
+  //     setProfiles(response.data);
+  //   });
+  //   console.log(name)
+  // }, []);
+
+  return (
+   <>
+      <Header />
+
+      <Container>
+        <header>
+          <div className="user">
+            <div className="circle">
+              <ChatCircle size={32} color="#B9CBD3" />
+            </div>
+
+            <hgroup>
+              <h1>Botname</h1>
+              <h4>id: botname</h4>
+            </hgroup>
+          </div>
+
+          <p>Created at 11/09/2019</p>
+        </header>
+
+        <Grid>
+          <div className='item1'>
+            <div>
+              <p>Region and idiom</p>
+              <p>EUA - English (EN)</p>
+            </div>
+
+            <div>
+              <p>Timezone</p>
+              <p>(UTC - 03:00) Brasília</p>
+            </div>
+          </div>
+
+          <div className='item2'>
+            <Circle color='lightBlue'>
+              <img src={iconUserActive} alt="" />
+            </Circle>
+
+            <hgroup>
+              <h2>1.000</h2>
+              <h3>Usuários ativos</h3>
+            </hgroup>
+          </div>
+          
+          <div className='item3'>
+            <Circle color='green'>
+              <img src={iconMessageReceived} alt="" />
+            </Circle>
+
+            <hgroup>
+              <h2>1.000</h2>
+              <h3>Mensagens recebidas</h3>
+            </hgroup>
+          </div>
+
+          <div className='item4'>
+            <Circle color='darkBlue'>
+              <img src={iconMessageSend} alt="" />
+            </Circle>
+
+            <hgroup>
+              <h2>1.000</h2>
+              <h3>Mensagens enviadas</h3>
+            </hgroup>
+          </div>
+
+          <div className='item5'>
+            <img src={illustration} alt="" />
+
+            <p>Status account</p>
+            <span>Free  </span>
+
+            <button>Update account</button>
+          </div>
+        </Grid>
+
+        <Footer>
+          <p>©2019, BLiP Todos os direitos reservados | Termos de Uso</p>
+        </Footer>
+      </Container>  
+   </>
+  );
 }
-
-export default Profile;
--- src/pages/profile/style.ts
+++ src/pages/profile/style.ts
@@ -1,8 +1,235 @@
 import styled from 'styled-components';
 
+interface Props {
+  color: string;
+}
+
 export const Container = styled.div`
-  background-color: #1b252d;
-  color: white;
-  height: 100vh;
+  max-width: 1600px;
   width: 100%;
+  height: 85vh;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding-bottom: 50px;
+
+    border-bottom: 1px solid #B9CBD3;
+  }
+
+  header .user {
+    display: flex;
+    align-items: center;
+  }
+
+  header .circle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 56px;
+    height: 56px;
+
+    background-color: #DEE8EC;
+    border-radius: 28px;
+
+    margin-right: 10px;
+  }
+
+  h1 {
+    font-size: 24px;
+    font-weight: 700;
+    color: #56616E;
+  }
+
+  h4 {
+    font-size: 16px;
+    font-weight: 400;
+    color: #8CA0B3;
+  }
+
+  p {
+    font-size: 16px;
+    font-weight: 400;
+    color: #8CA0B3;
+  }
+`;
+
+export const Grid = styled.div`
+  margin-top: 30px;
+
+  display: grid;
+  grid-template:
+		"item1 item2 item2 item5" 1fr
+		"item3 item3 item4 item5" 1fr 
+    / 1fr 1fr 1fr 1fr;
+  
+    grid-gap: 20px;
+
+    /* Estilização geral dos blocos */
+    .item1,
+    .item2,
+    .item3,
+    .item4 {
+      background-color: #fff;
+      box-shadow: 0px 2px 12px rgba(96, 123, 153, 0.15);
+      border-radius: 7px;
+
+      padding: 30px;
+    }
+
+    .item2,
+    .item3,
+    .item4 {
+      display: flex;
+      align-items: center;
+    }
+
+  .item1 {
+    grid-area: item1;
+  }
+
+  .item2 {
+    grid-area: item2;
+  }
+
+  .item3 {
+    grid-area: item3;
+  }
+
+  .item4 {
+    grid-area: item4;
+  }
+
+  h2 {
+    font-size: 24px;
+    font-weight: 800;
+    color: #52636C;
+  }
+
+  h3 {
+    font-size: 14px;
+    font-weight: 400;
+    color: #52636C;
+  }
+
+
+  /* Estilização do bloco apenas com textos */
+  .item1 div:first-of-type {
+    margin-bottom: 10px;
+  }
+
+  .item1 p {
+    font-size: 12px;
+    font-weight: 600;
+    color: #52636C;
+  }
+
+
+  /* Estilização do bloco com imagem, textos e botão */
+  .item5 {
+    grid-area: item5;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .item5 p {
+    font-size: 16px;
+    font-weight: 400;
+    color: #8CA0B3;
+
+    margin-top: 30px;
+  }
+
+  .item5 span {
+    font-size: 24px;
+    font-weight: 700;
+    color: #56616E;
+  }
+
+  .item5 button {
+    background: #2CC3D5;
+    border-radius: 8px;
+    color: #F8FBFB;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: 700;
+    padding: 10px;
+
+    margin-top: 20px;
+  }
+
+  .item5 button:hover {
+    background-color: #218F9C;
+  }
+
+  @media(max-width: 1100px) {
+    grid-template:
+      "item1" 1fr
+      "item2" 1fr
+      "item3" 1fr
+      "item4" 1fr
+      "item5" 1fr
+      / 1fr;
+
+    .item1,
+    .item2,
+    .item3,
+    .item4,
+    .item5 {
+      margin: 0 10px;
+      padding: 10px;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .item1 {
+      flex-direction: column;
+      text-align: center;
+    }
+  }
+`;
+
+export const Circle = styled.div<Props>`
+  width: 56px;
+  height: 56px;
+
+  background-color: ${(props) => props.color === 'lightBlue' && '#2CC3D5'};
+  background-color: ${(props) => props.color === 'darkBlue' && '#517BF2'};
+  background-color: ${(props) => props.color === 'green' && '#4DCB7B'};
+
+  border-radius: 28px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-right: 15px;
+`;
+
+export const Footer = styled.div`
+  border-top: 1px solid #B9CBD3;
+
+  padding-top: 50px;
+  padding-bottom: 10px;
+
+  p {
+    font-size: 12px;
+    font-weight: 400;
+    color: #738192;
+
+    text-align: center;
+  }
 `;
