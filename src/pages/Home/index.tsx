a7a68d5d085ae87efb96f86f14580b2602b7dbee
Bruno Netto <brunonettomac@outlook.com>
2022-09-02 21:51:43 -0300

    feat(homepage): adds filter by date functionality

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,6 +15,7 @@ export default function Home() {
   const [typeInfoExibition, setTypeInfoExibition] = useState('grid');
   const [profiles, setProfiles] = useState<ItemsData[]>([]);  
   const [search, setSearch] = useState('')
+  const [filterDate, setFilterDate] = useState<ItemsData[]>([]); 
 
 
   async function getItems() {
@@ -23,13 +24,21 @@ export default function Home() {
     });
   }
 
-  useEffect(() => {
-    getItems();
-  }, []);
+  function filteredDate() {
+    const filter = profiles.sort(
+      (objA, objB) => Number(new Date(objB.created)) - Number(new Date(objA.created)),
+    );
+
+    setFilterDate(filter);
+  }
 
   const filtered = search.length > 0
     ? profiles.filter(profile => profile.name.includes(search))
-    : []
+    : [];
+
+  useEffect(() => {
+    getItems();
+  }, []);
 
   return (
    <>
@@ -40,17 +49,16 @@ export default function Home() {
           <h1>My chatbots</h1>
 
           <div className='actions'>
-            <form action="">
-              <input
-                type='text'
-                name='search'
-                placeholder='Buscar...'
-                onChange={e => setSearch(e.target.value)}
-                value={search} 
-              />
-              <button>Order by name</button>
-              <button>Order by creation</button>
-            </form>
+            <input
+              type='text'
+              name='search'
+              placeholder='Buscar...'
+              onChange={e => setSearch(e.target.value)}
+              value={search} 
+            />
+
+            <button className='button-action'>Order by name</button>
+            <button className='button-action' onClick={() => filteredDate()}>Order by creation</button>
 
             <div className='buttons-set-exibition'>
               <button onClick={() => setTypeInfoExibition('grid')}>
@@ -70,24 +78,33 @@ export default function Home() {
 
         {typeInfoExibition === 'grid' && (
           <Grid>
-            {search.length > 0 ? (
-                filtered.map(filter => {
-                  return (
-                    <Link to={`/profile?name=${filter.name}`}>
-                    <Circle size={56} />
-                    <h2>{filter.name}</h2>
-                    <h3>{filter.type}</h3>
-                  </Link>
-                  )
-                })
-            ) :
-            profiles?.map((profile) => (
+            {filterDate.length > 0 || search.length > 0 ? '' : profiles.map((profile) => (
               <Link to={`/profile?name=${profile.name}`}>
                 <Circle size={56} />
                 <h2>{profile.name}</h2>
                 <h3>{profile.type}</h3>
               </Link>
             ))}
+
+            {filterDate && search.length <= 0 ? filterDate.map((profile) => (
+              <Link to={`/profile?name=${profile.name}`}>
+                <Circle size={56} />
+                <h2>{profile.name}</h2>
+                <h3>{profile.type}</h3>
+              </Link>
+            )) : ''}
+
+            {search.length > 0 ? (
+                filtered.map(filter => {
+                  return (
+                    <Link to={`/profile?name=${filter.name}`}>
+                      <Circle size={56} />
+                      <h2>{filter.name}</h2>
+                      <h3>{filter.type}</h3>
+                    </Link>
+                  )
+                })
+            ): ''} 
           </Grid>
         )}
 
